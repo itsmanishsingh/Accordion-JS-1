563d8f94f4d56fcec7d2731a30ea23d5d9446123
itsmanishsingh <itsmanishsingh6@gmail.com>
2023-02-28 21:51:45 +0530

    Code Update

--- faq.js
+++ faq.js
@@ -1,48 +1,26 @@
-// const faqData = [
-//   {
-//     id: 1,
-//     question: "Who are we?",
-//     answer:
-//       "We enable upscaling careers through flexible, interactive and collaborative learning. We believe in building learning communities by bringing together mentors, young minds, and creators.",
-//   },
-//   {
-//     id: 2,
-//     question: "What do we do?",
-//     answer:
-//       "Building learning communities with Our Affordable & Competent Courses",
-//   },
-//   {
-//     id: 3,
-//     question: "Are the community classes boring?",
-//     answer: "No, not at all!!!",
-//   },
-// ];
-
-// const accordianBody;
-// const faqs = [];
-
-// function showFaq() {
-
-// }
-
-// function createFaq() {
-
-// }
-
-// function btnStatusUpdate() {
-
-// }
-
 let heading = document.querySelectorAll(".accordion h3 ");
 
 heading.forEach((element) => {
   element.addEventListener("click", () => {
     let expended = element.nextElementSibling;
-    if(expended.style.display === "none"){
-        expended.style.display = "block"
-    }
-    else{
-        expended.style.display = "none";
+    if (expended.style.display === "none") {
+      expended.style.display = "block";
+    } else {
+      expended.style.display = "none";
     }
   });
 });
+
+// let button = document.querySelectorAll(".accordion h3");
+
+// button.forEach((element)=>{
+//   element.addEventListener("click",()=>{
+//     let expended = element.nextElementSibling;
+//     if(expended.style.display === "none"){
+//       expended.style.display = "block";
+//     }
+//     else{
+//       expended.style.display = "none";
+//     }
+//   });
+// });
